9e9be066e80cf734a45eefc8813cd7919bff35a6
PhysicalRevelry <kristinnewmancscs@gmail.com>
2020-10-21 13:45:23 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/physicalrevelry/new_camera_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.physicalrevelry.new_camera_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/landing_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Make a Choice'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: Text('Gallery'),
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child: Text("Camera"),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _decideImageView() {
+    if (imageFile == null) {
+      return Text("No selected Image");
+    } else {
+      return Image.asset(imageFile.path);
+    }
+  }
+
+  String _imagePath;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadImage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _decideImageView(),
+              RaisedButton(
+                onPressed: () {
+                  _showChoiceDialog(context);
+                },
+                child: Text("Select Image!"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  PickedFile imageFile;
+
+  void _openGallery(BuildContext context) async {
+    // ignore: deprecated_member_use
+    imageFile = await ImagePicker().getImage(source: ImageSource.gallery);
+    this.setState(() {});
+    Navigator.of(context).pop();
+  }
+
+  void _openCamera(BuildContext context) async {
+    // ignore: deprecated_member_use
+    imageFile = await ImagePicker().getImage(source: ImageSource.camera);
+    this.setState(() {});
+    Navigator.of(context).pop();
+  }
+
+
+  void loadImage() async {
+    SharedPreferences saveImage = await SharedPreferences.getInstance();
+    setState(() {
+      _imagePath = saveImage.getString("image path");
+    });
+  }
+}
